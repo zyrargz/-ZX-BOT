a292af0b8a77b4bbcc39a60749bd5fc0f36ea1ea
zyrargz <148509462+zyrargz@users.noreply.github.com>
2025-02-22 11:35:32 +0800

    test
    s

--- db.js
+++ db.js
@@ -1,13 +1,10 @@
 const { Client } = require('pg');
 
 const client = new Client({
-  connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false }
+  connectionString: "postgresql://postgres:yuyLdekTgrLnXafqvpeYlVaAWmPoSAsc@postgres-p9zd.railway.internal:5432/railway",
+  ssl: { rejectUnauthorized: false }  // Railway butuh SSL false
 });
 
-
 client.connect()
-  .then(() => console.log('✅ Database connected!'))
-  .catch(err => console.error('❌ Database connection error:', err));
-
-module.exports = client;
+  .then(() => console.log("✅ Database connected!"))
+  .catch(err => console.error("❌ Database connection error:", err));
